ff111b9fcf1e8e810974c5c52f5d00023556d31b
Rafaella Sampaio <sampaiorafaella@hotmail.com>
2024-04-17 22:35:59 -0400

    add connection

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # INFSCI-2710-FinalProject
+
 Final Project for Database Management Systems (INFSCI 2710)
+
+Arezou Farzaneh - arf111@pitt.edu
+Kun Zhang - kuz5@pitt.edu
+Rafaella Sampaio de Alencar - ras555@pitt.edu
---
+++ main.py
@@ -0,0 +1,27 @@
+import pymysql.cursors
+
+import os
+from dotenv import load_dotenv, dotenv_values 
+load_dotenv() 
+
+db_connection = pymysql.connect(host=os.getenv("HOST"),
+                             user=os.getenv("USER"),
+                             password=os.getenv("PASSWORD"),
+                             db='student_meal_express',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+
+    with db_connection.cursor() as cur:
+
+        cur.execute('SELECT * FROM recipes')
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+finally:
+
+    db_connection.close()
